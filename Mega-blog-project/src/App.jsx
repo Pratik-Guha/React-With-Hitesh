df4b4b7e3824aaf9a9167e0cc66e8be63aca5588
Pratik-Guha <sguha4966@gmail.com>
2023-10-02 12:16:57 +0530

    Mega-Blog-Project

---
+++ Mega-blog-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  console.log(import.meta.env.VITE_APPWRITE);
+  return (
+    <>
+      <h2>Mega Blog Project</h2>
+    </>
+  )
+}
+
+export default App
